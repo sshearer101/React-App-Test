0a5f108d2cc29fac3847c856222a991bbb1bc1f2
Sam Shearer <samshearer@Sams-MacBook-Pro.local>
2024-08-14 16:00:00 -0600

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
+// function App() {
+//   return "dfdsfds"
+// }
+
+// export default App;
+import "./App.css";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Sam's App
-        </a>
-      </header>
-    </div>
-  );
+return (
+<div className='App'>
+<header className='App-header'>
+<div class='alert alert-primary' role='alert'>
+<p style={{ display: "none" }} className='d-block'>
+Bootstrap is now successfully installed 😃
+</p>
+<p className='d-none'>
+Bootstrap is not installed if you can see this 😢
+</p>
+</div>
+</header>
+</div>
+);
 }
+
 
 export default App;
